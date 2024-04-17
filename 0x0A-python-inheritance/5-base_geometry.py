4e65b66a5dac146f036b6cc053c93b5ad9f7c014
Al1yu <harunaaliyu6449@gmail.com>
2024-04-17 02:40:41 -0700

    first

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Aliyu haruna <harunaaliyu6449@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
